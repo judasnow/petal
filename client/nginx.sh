ec6c6e1a4d5d07a6d9326c2d3e87f2430be0a911
judasnow <judasnow@gmail.com>
2013-07-10 21:01:59 +0800

    .

---
+++ client/nginx.sh
@@ -0,0 +1,12 @@
+#! /bin/bash   
+res_path="/home/www/petal/client"  
+mem_path="/dev/shm/petal_client"  
+lk_path="/home/www/petal_client"  
+ 
+if [ ! -d "$mem_path" ]; then  
+        cp -r "$res_path" "$mem_path"  
+fi  
+ 
+if [ ! -L "$lk_path" ]; then  
+        ln -s "$mem_path" "$lk_path"  
+fi
